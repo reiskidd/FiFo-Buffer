a2dfa944f00d04d0ee6bd74449986a39277ec939
reiskidd <79729768+reiskidd@users.noreply.github.com>
2022-06-30 23:10:19 +0200

    init

---
+++ Buffer/FiFo.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace Buffer
+{
+    public class FiFo
+    {
+        private readonly int[] buffer;
+
+        public bool IsFull { get; private set; }
+        public bool IsEmpty { get; private set; } = true;
+        public bool HasOverflow { get; private set; }
+        public int Tail { get; private set; } = -1;
+        public int Head { get; private set; } = -1;
+        public int Size { get { return buffer.Length; } }
+
+        public FiFo()
+        {
+            buffer = new int[10];
+        }
+
+        public FiFo(int size)
+        {
+            buffer = new int[size];
+        }
+
+        public void Clear()
+        {            
+            IsEmpty = true;
+            IsFull = false;
+            HasOverflow = false;
+            Head = -1;
+            Tail = -1;
+        }
+
+        public void Add(int value)
+        {
+            Head++;
+            if ((Head == (Size - 1) && Tail == 0) || Head == (Tail - 1))
+            {
+                IsFull = true;
+            }
+
+            if (Head == Size)
+            {
+                Head = 0;
+            }
+            buffer[Head] = value;
+
+            bool HeadEqualsTail = Head == Tail;
+            if (IsEmpty || HeadEqualsTail)
+            {
+                Tail++;
+                if (HeadEqualsTail)
+                {
+                    HasOverflow = true;
+                }
+
+                if (Tail == Size)
+                {
+                    Tail = 0;
+                }                
+            }
+            IsEmpty = false;
+        }
+
+        public int GetValue()
+        {
+            if (IsEmpty)
+            {
+                Console.WriteLine("Error");
+                return -1;
+            }
+
+            int value = buffer[Tail];
+            if (Tail == Head)
+            {
+                Clear();
+                return value;
+            }
+
+            Tail++;
+            HasOverflow = false;
+            IsFull = false;
+
+            if (Tail == Size)
+            {
+                Tail = 0;
+            }
+
+            return value;
+        }
+
+        public int[] GetStack()
+        {
+            if (IsEmpty)
+            {
+                return new int[0];
+            }
+
+            int[] ausgabeArray = new int[buffer.Length];
+            int counter = 0;
+
+            if (Tail <= Head)
+            {
+                for (int k = Tail; k <= Head; k++)
+                {
+                    ausgabeArray[counter] = buffer[k];
+                    counter++;
+                }
+            }
+            else
+            {
+                for (int i = Tail; i < Size; i++)
+                {
+                    ausgabeArray[counter] = buffer[i];
+                    counter++;
+                }
+
+                for (int j = 0; j <= Head; j++)
+                {
+                    ausgabeArray[counter] = buffer[j];
+                    counter++;
+                }
+            }
+
+            int[] ausgabe = new int[counter];
+
+            for (int i = 0; i < ausgabe.Length; i++)
+            {
+                ausgabe[i] = ausgabeArray[i];
+            }
+
+            return ausgabe;
+        }
+    }
+}
---
+++ Buffer/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Buffer
+{
+    internal class Program
+    {
+        private static FiFo buffer;
+        
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Wie viel Plätze soll dein Array haben?");
+            bool userBool = int.TryParse(Console.ReadLine(), out int userEingabe);
+
+            if (userBool == true)
+            {
+                buffer = new FiFo(userEingabe);
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Eingabe ungültig.\nHöchster Index ist 10.\n");
+                buffer = new FiFo();
+            }
+
+            Menue();
+            Console.ReadLine();
+        }
+        
+
+        private static void Menue()
+        {
+            bool schleife = true;
+            while (schleife)
+            {
+                Console.WriteLine("Optionen:\nClear\nAdd\nGet value\nGet pointer\nAusgabe\nExit\n");
+                string userEingabe = Console.ReadLine();
+
+                if (userEingabe == "Clear")
+                {
+                    buffer.Clear();
+                }
+                else if (userEingabe == "Add")  
+                {
+                    Console.WriteLine("Füttere den Buffer mit einer Zahl\n");
+                    bool inputIsNumber = int.TryParse(Console.ReadLine(), out int eingabe);
+                    if (inputIsNumber)
+                    {
+                        buffer.Add(eingabe);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ungültige eingabe!\n");
+                    }
+                    Console.Clear();
+                }
+                else if (userEingabe == "Get value")    
+                {
+                    Console.Clear();
+                    int zahlAusgabe = buffer.GetValue();
+                    Console.WriteLine("Dein Tailwert ist: " + zahlAusgabe + "\n");
+                }
+                else if (userEingabe == "Get pointer")
+                {
+                    Console.WriteLine("Head-index: " + buffer.Head);
+                    Console.WriteLine("Tail-index: " + buffer.Tail + "\n");
+                }
+                else if (userEingabe == "Ausgabe")  
+                {
+                    Console.Clear();
+                    int[] ausgabe = buffer.GetStack();
+
+                    foreach (int item in ausgabe)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+                else if (userEingabe == "Exit")
+                {
+                    schleife = false;
+                }
+                else
+                {
+                    Console.WriteLine("Option eingabe ist ungültig!\n");
+                }                
+            }
+        }
+    }
+}
